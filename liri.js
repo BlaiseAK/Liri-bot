9bf83438a84f830c5ce29b29583d6ad10db69e25
BlaiseAK <blaise.a.kress@gmail.com>
2018-06-28 16:03:38 -0400

    got the my-tweets, spotify-this-song, and movie-this functions all working. do-the-thing is the last that is needed

--- liri.js
+++ liri.js
@@ -24,38 +24,66 @@ var a = process.argv[2];
 for (var i = 3; i < nodeArgs.length; i++) {
 
     if (i > 3 && i < nodeArgs.length) {
-  
-      doTheThing = doTheThing + "+" + nodeArgs[i];
-  
+
+        doTheThing = doTheThing + "+" + nodeArgs[i];
+
     }
-  
+
     else {
-  
-      doTheThing += nodeArgs[i];
-  
+
+        doTheThing += nodeArgs[i];
+
     }
-  }
+}
 
 
 if (a === 'my-tweets') {
-    var params = {blaiseiscool1: "node.js", count: 20}
+    var params = { blaiseiscool1: "node.js", count: 20 }
     // show tweets
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if(!error) {
-            tweets.forEach(function(tweet) {
-                console.log(tweet.text);
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function (tweet) {
+
+                console.log("\n===============================\n");
+                console.log("Twitter handle: " + tweet.user.screen_name)
+                console.log("Tweet: " + tweet.text);
+                console.log("Posted on: " + tweet.created_at);
+                console.log("\n===============================\n");
             })
-        
-        
-    }  
+
+
+        }
     });
 }
 if (a === 'spotify-this-song') {
+    // if doTheThing variable is blank then....
+    if (doTheThing = '') {
+        doTheThing = 'The Sign';
+        // look at process.argv[3] for the song info
+        spotify.search({ type: 'track', query: doTheThing })
+            .then(function (response) {
 
-    // look at process.argv[3] for the song info
-    spotify.search({ type: 'track', query: doTheThing})
-        .then(function(response) {
-            console.log("===============================");
+                console.log("\n===============================\n");
+                // Artist
+                console.log(response.tracks.items[0].artists[0].name);
+                // Song name
+                console.log(response.tracks.items[0].name);
+                // preview link from Spotify
+                console.log(response.tracks.items[0].external_urls.spotify);
+                // The album
+                console.log(response.tracks.items[0].album.name);
+                console.log("\n===============================\n");
+
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+    // else look at doTheThing for the song info
+    spotify.search({ type: 'track', query: doTheThing })
+        .then(function (response) {
+
+            console.log("\n===============================\n");
             // Artist
             console.log(response.tracks.items[0].artists[0].name);
             // Song name
@@ -64,35 +92,39 @@ if (a === 'spotify-this-song') {
             console.log(response.tracks.items[0].external_urls.spotify);
             // The album
             console.log(response.tracks.items[0].album.name);
-            console.log("===============================");
+            console.log("\n===============================\n");
 
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
         });
 }
 if (a === 'movie-this') {
+    if (doTheThing === '') {
+        doTheThing = 'Mr. Nobody';
+    }
+
     // look at process.argv[3] for the movie info
     var queryUrl = "http://www.omdbapi.com/?t=" + doTheThing + "&y=&plot=short&apikey=trilogy";
     console.log(queryUrl);
-    request(queryUrl, function(error, response, body) {
+    request(queryUrl, function (error, response, body) {
 
         // If the request is successful
         if (!error && response.statusCode === 200) {
-      
-          // give me my stuff like this
-          console.log("===============================");
-          console.log("Title: "+JSON.parse(body).Title);
-          console.log("Year of release: "+JSON.parse(body).Year);
-          console.log("IMDB Rating: "+JSON.parse(body).imdbRating);
-          console.log("Rotten Tomatoes Rating: "+JSON.parse(body).Ratings[1][1]);
-          console.log("Country produced in: "+JSON.parse(body).Country);
-          console.log("Languages: "+JSON.parse(body).Language);
-          console.log("Plot: "+JSON.parse(body).Plot);
-          console.log("Actors: "+JSON.parse(body).Actors);
-          console.log("===============================");
+
+            // give me my stuff like this
+            console.log("\n===============================\n");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year of release: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1][1]);
+            console.log("Country produced in: " + JSON.parse(body).Country);
+            console.log("Languages: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("\n===============================\n");
         }
-      });
+    });
 
 }
 if (a === 'do-what-it-says') {
